f6d05103e8a9206f69bb018c9bff9f9e4bf1625a
Abhinav <abhinavgupta24@gmail.com>
2023-06-04 18:56:10 +0530

    add existing project files

---
+++ client/src/dummyData.js
@@ -0,0 +1,27 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "person/6a.jpeg",
+    username: "Abhinav Gupta",
+  },
+  {
+    id:2,
+    profilePicture: "person/2.jpeg",
+    username: "Ayush kumar verma",
+  },
+  {
+    id:3,
+    profilePicture: "person/3.jpeg",
+    username: "Abhishek garg",
+  },
+  {
+    id:4,
+    profilePicture: "person/4.jpeg",
+    username: "Yash dwivedi",
+  },
+  {
+    id:5,
+    profilePicture: "person/5.jpeg",
+    username: "Arpit srivastava",
+  },
+];
